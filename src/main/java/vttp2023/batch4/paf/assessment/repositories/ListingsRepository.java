9195aca17fc2c10b85e635ff57ca94d0c94ed0b7
chloetohce <chloetohce@gmail.com>
2025-02-11 00:47:38 +0800

    task 4

--- src/main/java/vttp2023/batch4/paf/assessment/repositories/ListingsRepository.java
+++ src/main/java/vttp2023/batch4/paf/assessment/repositories/ListingsRepository.java
@@ -1,10 +1,12 @@
 package vttp2023.batch4.paf.assessment.repositories;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -37,14 +39,40 @@ public class ListingsRepository {
 	}
 
 	/*
-	 * Write the native MongoDB query that you will be using for this method
-	 * inside this comment block
-	 * eg. db.bffs.find({ name: 'fred }) 
-	 *
-	 *
+	 *	db.listings.find({
+	 		'address.suburb': {$regex: <suburb>, $options: 'i'},
+			price: {$lte: 250},
+			accommodates: {$gte: 2},
+			min_nights: {$lte: 5}
+	 	}).projection({
+			name: 1, accommodates:1 1, price: 1
+		}).sort({price: -1})
+		
 	 */
 	public List<AccommodationSummary> findListings(String suburb, int persons, int duration, float priceRange) {
-		return null;
+		Criteria criteria = Criteria.where("address.suburb")
+			.regex(suburb, "i")
+			.and("price").lte(priceRange)
+			.and("accommodates").gte(persons)
+			.and("min_nights").lte(duration);
+		Query query = Query.query(criteria);
+		query.fields()
+			.include("_id", "name", "accommodates", "price");
+		query = query.with(Sort.by(Sort.Direction.DESC, "price"));
+
+		List<Document> results = template.find(query, Document.class, "listings");
+		List<AccommodationSummary> accoms = new ArrayList<>();
+
+		for (Document d: results) {
+			AccommodationSummary summary = new AccommodationSummary();
+			summary.setId(d.getString("_id"));
+			summary.setName(d.getString("name"));
+			summary.setAccomodates(d.getInteger("accommodates"));
+			summary.setPrice(d.get("price", Number.class).floatValue());
+			accoms.add(summary);
+		}
+		
+		return accoms;
 	}
 
 	// IMPORTANT: DO NOT MODIFY THIS METHOD UNLESS REQUESTED TO DO SO
